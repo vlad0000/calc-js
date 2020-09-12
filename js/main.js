b070214f45d811a3732523170a062c19cef23c22
vlad0000 <52610344+vlad0000@users.noreply.github.com>
2020-09-12 19:03:18 +0300

    v 1.0.0
    Имеет приятный дизайн
    
    Калькулятор умеет / * + -
    
    Можно добавить приоритет()
    
    Умеет работать с отрицательными и дробными числами

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$(document).ready(function() {
-  
-});
+let form = document.forms.form,
+      input = form.textview,
+      clear = document.querySelector('.clear');
+      equal
+
+function clean(input){
+  input.value = '';
+}value
+function insert(num, input) {
+  input.value += num;
+}
+function back(input){
+  input = input.value.substring(0, input.value.length - 1);
+}
+function equal(input) {
+  if(input.value){
+    input.value = eval(input.value);
+  }
+}
